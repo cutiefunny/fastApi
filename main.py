9d7470f4484b1fc1bd110d4adf522c99fe600abc
cutiefunny <cutiefunny@gmail.com>
2023-06-06 15:18:34 +0900

    test3

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/test")
-async def root():
+async def test():
     return "test"
 
 @app.get("/test2")
-async def root():
+async def test2():
     return "test2"
+
+@app.get("/test3")
+async def test3():
+    return "test3"
